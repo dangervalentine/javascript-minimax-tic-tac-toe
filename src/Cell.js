09167c2aefce46f6d00a6809fc78cf396189b94d
vic <victordjohnson@gmail.com>
2019-11-16 21:20:51 -0600

    replaced the 'x' and 'o' with svgs for more control over their placements within the cells

--- src/Cell.js
+++ src/Cell.js
@@ -1,13 +1,20 @@
 import React from "react";
 
+import oh from "./oh.svg";
+import ex from "./ex.svg";
+
 function Cell(props) {
-  const character = props.value === 0 ? "" : props.value === 2 ? "○" : "×";
+  const background = props.value === 0 ? "" : props.value === 2 ? oh : ex;
+
   return (
     <div
       className={`cell ${props.winningPeice ? props.winningAlignment : ""}`}
       onClick={() => props.insert(props.index, true)}
     >
-      <span className={`${props.value === 2 ? "oh" : "ex"}`}>{character}</span>
+      <span
+        style={{ backgroundImage: `url(${background})` }}
+        className={`${props.value === 2 ? "oh" : "ex"}`}
+      ></span>
     </div>
   );
 }
